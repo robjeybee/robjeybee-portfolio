0787f2ef81915962cd820418bd6f08022a02ad1c
Rob B <rob@rjrobbie.dev>
2023-11-14 21:44:18 +0000

    added linear gradient to svg path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/footer.scss'
+import Socials from './Socials'
 
 const Footer = () => {
   return (
@@ -7,14 +8,16 @@ const Footer = () => {
     <footer className='footer'>
       <div className='footer-content'>
         <div className='footer-content-left'> 
-        <img src="/icons/robjeybee-square-logo.svg" alt="" />
+          <img src="/icons/robjeybee-square-logo.svg" alt="" />
         <div>
           <p>Rob Barber</p>
           <p>Junior Developer</p>
         </div>
+        <Socials />
        </div>
        <div className='footer-content-right'> 
-       <p><i class="fa-regular fa-copyright"></i> 2023 robjeybee.com</p>
+       <p><i class="fa-regular fa-copyright"></i> 2023</p>
+       <p>robjeybee.com</p>
           <p>made with curiosity and lightning bolts</p>
        </div>
        
---
+++ src/components/Socials.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/socials.scss'
+
+const Socials = () => {
+  return (
+    <div>
+       <div className='footer-social-icons'>
+          <a href=""><i class="fa-brands fa-linkedin"></i></a>
+          <a href=""><i class="fa-brands fa-x-twitter"></i></a>
+          <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+          <a href=""><i class="fa-brands fa-dev"></i></a>
+          <a href=""><i class="fa-brands fa-hashnode"></i></a>
+          <a href=""><i class="fa-brands fa-spotify"></i></a>
+        </div>
+    </div>
+  )
+}
+
+export default Socials
