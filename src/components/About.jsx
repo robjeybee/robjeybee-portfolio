b96aaf0c2f602ecbf55c3ef76ee773a3bbc0cb51
Rob B <rob@rjrobbie.dev>
2023-11-27 21:40:28 +0000

    added about info and blog content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,38 @@ import "../styles/about.scss";
 const About = () => {
   return (
     <div>
-      <div class="about">
-        <div class="about-content">
+      <div className="about">
+        <div className="about-content">
           <h3>About me.</h3>
-          <p>Some lorem ipsum stuff about me</p>
-          <p>Some lorem ipsum stuff about me</p>
-          <p>Some lorem ipsum stuff about me</p>
-          <p>Some lorem ipsum stuff about me</p>
+          <div className="about-content-copy">
+            <p>
+              I'm press officer by day who's career switching into tech by
+              trading in headlines for lines of code at night.
+            </p>
+
+            <p>
+              Having cut my teeth at the Command Shift bootcamp, I've not only
+              embraced but strived for clean code and the latest dev tools in
+              the dynamic world of software development.
+            </p>
+
+            <p>
+              But wait, there's more! Have you heard the Codewords podcast?
+              That's me - your friendly neighbourhood host, with a backstage
+              pass to the thoughts and strategies of amazing developers from
+              around the globe.
+            </p>
+
+            <p>
+              Feeling curious or just fancy a digital coffee chat? Don't
+              hesitate to drop me a line. Let's connect, swap stories, and maybe
+              even crack some code together.
+            </p>
+          </div>
         </div>
         <div class="about-img">
           <img
-            src="/images/project-placeholder-image.svg"
+            src="/images/robjeybee-pink-square.svg"
             alt="Project Placeholder Image of a yellow lightning bolt on a pink background"
           />
         </div>
