76dbf3404dc92345405e9a561f120537bdfebc24
Rob B <rob@rjrobbie.dev>
2024-02-15 20:11:57 +0000

    used useState to check if the button is clicked to create a popup button effect, and enabled the input fields to clear on click

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../styles/contact.scss";
 import "../styles/socials.scss"
 import Socials from "./Socials";
@@ -22,8 +22,20 @@ const Contact = () => {
         (error) => {
           console.log('FAILED...', error.text);
         },
-      );
+    );
+    
+    form.current.reset();
   };
+
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+     console.log("Hello World");
+    setIsClicked(!isClicked);
+       setTimeout(() => {
+      setIsClicked(false);
+    }, 300); 
+  }
 
   return (
      <div className="contact">
@@ -52,12 +64,14 @@ const Contact = () => {
           </div>
  <form className="contact-form" ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" placeholder="Your name"/>
+      <input type="text" name="user_name" placeholder="Your name" required="required"/>
       <label>Email</label>
-      <input type="email" name="user_email" placeholder="Your email"/>
+      <input type="email" name="user_email" placeholder="Your email" required="required"/>
       <label>Message</label>
       <textarea name="message" placeholder="Let me talk to ya! Yeah!"/>
-      <input className="contact-button" type="submit" value="Send"/>
+      <button onClick={handleClick}  className={`contact-button ${isClicked ? 'reverse-hover' : '' && (
+        <p>Email sent!</p>
+      )}`} type="submit" value="Send" required="required" >Submit</button>
     </form>
         </div>
       </div>
