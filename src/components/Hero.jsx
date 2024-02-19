d1595b2da7cd0b91f66d8c271973142acb6e29c8
Rob B <rob@rjrobbie.dev>
2024-02-19 20:21:49 +0000

    removed unused svg and wrote and deleted code until I was back to where I had started from, that was not the plan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <p>Hi my name's Rob</p>
         <h1>I'm a junior developer</h1>
           <p>I build websites like this one in React and more. I'm making a career switch into tech from public relations.</p>
-          <button  className="hero-button">Find out more</button>
+          <button  className="hero-button">Let's talk →</button>
       </div>
        <div className="hero-content-img">
         <img className="hero-img" src="/images/robjeybee-blue.svg" alt="" />
