a2bd5e7a67d60d0c2bc70460d287351ec5a25d3f
Rob B <rob@rjrobbie.dev>
2025-01-30 20:31:41 +0000

    corrected image file path in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className='hero' id="home">
                 <div className="hero-content">
                     <div className="hero-intro-text">
-                              <img className="hero-img" src="/public/Rob .png" alt="" />
+                              <img className="hero-img" src="/Rob .png" alt="" />
                     <h1>Hey I’m Rob, and I am a junior fullstack developer.</h1>
                     <p>Career switching into tech with a background in public relations and communications - specialising in React and JavaScript.</p>
                     <a href="#">Fancy a chat? Let's talk</a>
